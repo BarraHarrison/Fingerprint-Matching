5aa2226fc08d777ea952217d086e6cb42d217e4d
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-02 14:45:20 +0900

    Resizing the sample

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 # Fingerprint Matching in Python
 import os 
 import cv2
+
+sample = cv2.imread("SOKOFingerprints/SOCOFing/Altered/Altered-Hard/150__M__Right_index_finger_Obl.BMP")
+sample = cv2.resize(sample, None, fx=2.5, fy=2.5)

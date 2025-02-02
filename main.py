0ae2e4bf3fa9767629790becd4fbc4eeaccf7464
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-02 14:50:13 +0900

    imshow shows us the current fingerprint sample that I have chosen from the dataset

--- main.py
+++ main.py
@@ -2,5 +2,9 @@
 import os 
 import cv2
 
-sample = cv2.imread("SOKOFingerprints/SOCOFing/Altered/Altered-Hard/150__M__Right_index_finger_Obl.BMP")
+sample = cv2.imread("SOKOFingerprints/SOCOFing/Altered/Altered-Hard/49__M_Right_index_finger_Obl.BMP")
 sample = cv2.resize(sample, None, fx=2.5, fy=2.5)
+
+cv2.imshow("Sample", sample)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
